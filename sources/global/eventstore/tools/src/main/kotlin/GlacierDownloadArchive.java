262758d6ffadee66e5ab154b83fb0bced7474366
vach <vachagan.balayan@gmail.com>
2016-06-13 07:55:23 +0800

    implementing StreamUploader

---
+++ sources/global/eventstore/tools/src/main/kotlin/GlacierDownloadArchive.java
@@ -0,0 +1,40 @@
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+import com.amazonaws.services.glacier.AmazonGlacierClient;
+import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GlacierDownloadArchive {
+    public static String vaultName = "event-store";
+    public static String archiveId = "Hma-C6bYB7OHpYLztliD47UHVhreAbiemBIrLe6AGI8h8ulUxX7Lw9yBNIAZoaDnhqLFFPag4IDRS1tHjbSnnvvxxqRdW1GNueDaSDdFkRGAtQmsOsnjJ3NkFF7EWWkGKz3B_bmUFg";
+    public static String downloadFilePath  = "/Users/vach/workspace/jarvis/dist/temp";
+    
+    public static AmazonGlacierClient glacierClient;
+    public static AmazonSQSClient sqsClient;
+    public static AmazonSNSClient snsClient;
+    
+    public static void main(String[] args) throws IOException {
+        SystemPropertiesCredentialsProvider credentials = new SystemPropertiesCredentialsProvider();
+    	
+        glacierClient = new AmazonGlacierClient(credentials);        
+        sqsClient = new AmazonSQSClient(credentials);
+        snsClient = new AmazonSNSClient(credentials);
+        
+        glacierClient.setEndpoint("glacier.us-west-2.amazonaws.com");
+        sqsClient.setEndpoint("sqs.us-west-2.amazonaws.com");
+        snsClient.setEndpoint("sns.us-west-2.amazonaws.com");
+
+        try {
+            ArchiveTransferManager atm = new ArchiveTransferManager(glacierClient, sqsClient, snsClient);
+            
+            atm.download(vaultName, archiveId, new File(downloadFilePath));
+            
+        } catch (Exception e)
+        {
+            System.err.println(e);
+        }
+    }
+}
---
+++ sources/global/eventstore/tools/src/main/kotlin/GlacierUploadArchive.java
@@ -0,0 +1,33 @@
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+import com.amazonaws.services.glacier.AmazonGlacierClient;
+import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
+import com.amazonaws.services.glacier.transfer.UploadResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class GlacierUploadArchive {
+
+  public static String vaultName = "event-store";
+  public static String archiveToUpload = "/Users/vach/workspace/jarvis/turtlerules.pdf";
+
+  public static AmazonGlacierClient client;
+
+  public static void main(String[] args) throws IOException {
+    SystemPropertiesCredentialsProvider credProvider = new SystemPropertiesCredentialsProvider();
+
+    client = new AmazonGlacierClient(credProvider);
+    client.setEndpoint("https://glacier.us-west-2.amazonaws.com/");
+
+    try {
+      ArchiveTransferManager atm = new ArchiveTransferManager(client, credProvider);
+
+      UploadResult result = atm.upload(vaultName, "my archive " + (new Date()), new File(archiveToUpload));
+      System.out.println("Archive ID: " + result.getArchiveId());
+
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+  }
+}
